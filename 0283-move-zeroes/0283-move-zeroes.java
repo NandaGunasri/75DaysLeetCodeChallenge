d65a58ffe477303e9feccb4ac46571da2461d03c
NandaGunasri <nandagunasri@gmail.com>
2026-03-21 13:05:43 +0530

    Time: 2 ms (90.55%), Space: 47.8 MB (71.37%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0; // position to place next non-zero
+
+        // Step 1: move all non-zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        // Step 2: fill remaining positions with 0
+        while (j < nums.length) {
+            nums[j] = 0;
+            j++;
+        }
+    }
+} 
+    
